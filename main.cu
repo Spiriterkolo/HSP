20217b379939c1f828d43e9a28dd601d790635b4
Spiriterkolo <spiriter.delabarre@gmail.com>
2022-11-28 16:57:14 +0100

    init matrix and matrix print working well on cpu

--- main.cu
+++ main.cu
@@ -2,13 +2,32 @@
 #include <stdlib.h>
 
 void MatrixInit(float *M, int n, int p){
+    float max = RAND_MAX;
     for(int i=0; i<n; i++){
         for(int j=0; j<p; j++){
-            M[i][j] = (float)(rand()/RAND_MAX)*2-1;
+            M[j + i*p] = (rand() / max) * 2 - 1.0 ;
         }
     }
 }
 
+void MatrixPrint(float *M, int n, int p){
+    printf("Matrice : ");
+    for(int i=0; i<n; i++){
+        printf("\n");
+        for(int j=0; j<p; j++){
+            printf("%f ", M[j + i*p]);
+        }
+    }
+    printf("\n");
+}
+
 int main(){
+    int n = 4;
+    int p = 3;
+    float *M;
+    M = (float*)malloc((sizeof (float))* n * p);
+    MatrixInit(M, n, p);
+    MatrixPrint(M, n, p);
+    free(M);
     return 0;
 }
